c8c6d87ec653917efdc613b1c3a47ac940fa3ebe
Joar Christoffersen <jc@fjellsport.no>
2023-07-30 23:19:15 +0200

    switch sort order

--- src/components/FoodListForDay.jsx
+++ src/components/FoodListForDay.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function FoodListForDay({ foodItems }) {
+  // Sort the foodItems array in descending order based on the timestamp
+  const sortedFoodItems = foodItems.sort((a, b) => (a.timestamp < b.timestamp ? 1 : -1));
   return (
     <div className="listing-details">
       <div>
-        {foodItems.map((item) => (
+        {sortedFoodItems.map((item) => (
           <p key={item.id}>
             {item.name} - {item.quantity && <div>{item.quantity} stk</div>} {item.volume && <div>{item.volume} dl</div>}{' '}
             {item.weight && <div>{item.weight} g</div>}
